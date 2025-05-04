825ddb6a4b6f179f8d8c86c0cab9a0f17d3779c7
AnBLarsen <andrealarsen@protonmail.com>
2025-05-04 13:44:31 -0400

    added a .env.local to the front folder

--- front/src/context/UserContext.jsx
+++ front/src/context/UserContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useCallback, useState } from "react";
 import axios from "axios";
 
+const APIURL = import.meta.env.VITE_PUBLIC_API_URL;
+
 // eslint-disable-next-line react-refresh/only-export-components
 export const UsersContext = createContext({
     user: null, 
@@ -20,12 +22,12 @@ export const UsersProvider = ({ children }) => {
     
 
     const registerUser = async (userData) => {
-        await axios.post("http://localhost:3000/users/register", userData);
+        await axios.post(`${APIURL}/users/register`, userData);
     };
 
     const loginUser = async (loginData) => {
         
-        const response = await axios.post("http://localhost:3000/users/login", loginData);
+        const response = await axios.post(`${APIURL}/users/login`, loginData);
         setUser(response.data.user);  
         localStorage.setItem("user", JSON.stringify(response.data.user));
        
@@ -38,18 +40,18 @@ export const UsersProvider = ({ children }) => {
     };
 
     const bookAppointment = async (values) => {
-        await axios.post(`http://localhost:3000/appointments/schedule`, values);
+        await axios.post(`${APIURL}/appointments/schedule`, values);
         
     };
 
     const getUserAppointments = useCallback(async (userId) => {
-        const { data } = await axios.get(`http://localhost:3000/users/${userId}`);
+        const { data } = await axios.get(`${APIURL}/users/${userId}`);
         setUserAppointments(data.appointments);
         
     }, []); 
 
     const cancelAppointment = async (appointmentId) => {
-        await axios.put(`http://localhost:3000/appointments/cancel/${appointmentId}`)
+        await axios.put(`${APIURL}/appointments/cancel/${appointmentId}`)
         const updatedAppointments = userAppointments.map((appointment) => appointment.id === appointmentId ? {...appointment, status:"cancelled"}: appointment)
         setUserAppointments(updatedAppointments)
     }
